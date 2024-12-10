f616f607a6973087443c7736d0000565d99ee938
priyavarshini20 <priyavarshini383@gmail.com>
2024-12-10 15:21:02 +0530

    final initial commit

--- backend/api.js
+++ backend/api.js
@@ -1,58 +1,81 @@
 const express = require("express");
 const mongoose = require("mongoose")
-const cors = require("cors")
-
-const auth = require("./routes/auth.js")
-const house = require("./routes/house.js")
-const reservations = require("./routes/reservations.js")
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth.js")
+const houseRoute = require("./routes/house.js")
+const reservationsRoute = require("./routes/reservations.js")
 
 require('dotenv').config();
 
 
+
 const app = express();
 
-// parse Data
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+const connect = async () => {
 
-// Use routes
-app.use("/auth", auth);
-app.use("/house", house);
-app.use("/reservations", reservations)
-
-
-async function main() {
-    await mongoose.connect(`mongodb+srv://priya:Admin123@atlas.8fy2h.mongodb.net/motel-develpoment-db`)
     try {
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
-        console.log('MongoDB connected By Mongo Client priya!')
-    } catch (err) {
-        console.log(err)
+         await mongoose.connect(process.env.mongodb);
+        //await mongoose.connect("process.env.MONGO");
+    } catch (error) {
+        throw error;
     }
-}
+};
+    
 
-app.get('5001/',(req,res)=>{
-    res.send(` Hello Express is server Working on ${process.env.PORT}`);
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected!");
 })
-app.use(cors({
-    origin:'http:localhost:3001',
-    credentials:true
-}))
 
-// add middleware to parse cookies
-//app.use(cookieParser());
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDB connected!");
+})
 
-
-// add middleware to log requests
-// app.use(morgan('dev'));
-
-app.use('/', express.static('dist'))
-app.get("/",(req,res)=>{
-    res.json("welcome hotel")
-    })
+app.get("/", (req, res)=>{
+    res.send("hello first request!")
+});
 
 
-main();
+
+
+
+
+
+
+// middlewares               - to prevent too much code in index.js so separate route folder is created.
+// creating middleware
+
+app.use(cors({origin:"http://localhost:5001",method:"GET"}))
+// app.use(cors)
+
+//to use json - sending by the user in post request
+app.use(express.json())
+app.use(cookieParser());
+
+app.use("/api/v1", authRoute);
+app.use("/api/v1", houseRoute);
+app.use("/api/v1", reservationsRoute);
+
+
+//Error Handler Middleware 
+app.use((err, req, res, next)=>{
+
+    const errorStatus = err.status || 500
+    const errorMessage = err.errorMessage || "Something went wrong!"
+
+    //handling error and responding to the client request. (sending error to client side)
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        messsage: errorMessage,
+        stack: err.stack,                           //it will give more detail about stack
+    });
+        
+    // next(); // next is callback function
+    
+});
+
+app.listen(5001, ()=> {
+    connect();
+    console.log("Connected to Backend!")
+})
